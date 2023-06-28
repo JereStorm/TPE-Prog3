13746f1b366a9d0abc1079ec5d52d9b8315711a7
lautarovg02 <lautarovg147@gmail.com>
2023-06-27 22:06:56 -0300

    [New]: Se agrego resolucion al problema con algoritmo Greedy

--- DatasetMain.java
+++ DatasetMain.java
@@ -8,19 +8,41 @@ public class DatasetMain {
 
         public static void main(String[] args) {
             // PATH/AL/ARCHIVO
-            String path = "src/Datasets/dataset2.txt";
+            String path = "./Datasets/dataset2.txt";
             CSVReader reader = new CSVReader(path);
 
             ArrayList<Arco<Integer>> arcosDataSet  = reader.getArcos();
-            
-            Backtracking backtracking = new Backtracking(arcosDataSet);
-            
-            System.out.println("Backtracking");
-            List<Arco<Integer>> solucion = backtracking.backtracking();
-            
-            System.out.println("Data set main SOLUCION: "+solucion);
-            System.out.println( "Kilometros totales: "+backtracking.getKmTotales());
-            System.out.println( "Metrica: " + backtracking.getMetrica());
+
+            Greedy greedy = new Greedy(arcosDataSet);
+
+            System.out.println("Greedy");
+            List<Arco<Integer>> solucion = greedy.greedy();
+            System.out.print("Data set main SOLUCION: " + solucion);
+
+            System.out.print(";");
+            System.out.println(" ");
+            System.out.println( "Kilometros totales: "+greedy.getKmActuales());
+            System.out.println( "Metrica: " + greedy.getMetrica());
+            /*
+            * Complejidad:
+            * A la hora de calcualr la complejidad , SERA 2 a la n - 1,
+            * pq si tenemos 4 estaciones con un minimo de 3 conexiones,
+            *  ya alcanza y no vamos a explorar mas q eso*/
+
+//            Backtracking backtracking = new Backtracking(arcosDataSet);
+//
+//            System.out.println("Backtracking");
+//            List<Arco<Integer>> solucion = backtracking.backtracking();
+//            System.out.print("Data set main SOLUCION:");
+//            for(Arco<Integer> a : solucion ){
+//                int origen = a.getVerticeOrigen();
+//                int destino = a.getVerticeDestino();
+//                System.out.print("E" + origen + "-E" + destino + ",");
+//            }
+//            System.out.print(";");
+//            System.out.println(" ");
+//            System.out.println( "Kilometros totales: "+backtracking.getKmTotales());
+//            System.out.println( "Metrica: " + backtracking.getMetrica());
 
 
         }
---
+++ Greedy.java
@@ -0,0 +1,99 @@
+import Grafo.Arco;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Greedy {
+
+    private UnionFind unionFind;
+    private List<Integer> estaciones;
+
+    private List<Arco<Integer>> candidatos;
+
+    private Integer kmActuales, metrica;
+
+    public Greedy(ArrayList<Arco<Integer>> candidatos){
+        this.candidatos = candidatos;
+        this.kmActuales = 0;
+        this.metrica = 0;
+        this.estaciones = new ArrayList<>();
+        this.getEstaciones();
+    }
+
+    public Integer getMetrica() {
+        return this.metrica;
+    }
+
+    public Integer getKmActuales() {
+        return this.kmActuales;
+    }
+
+    public List<Arco<Integer>> greedy() {
+        List<Arco<Integer>> solucion = new ArrayList<>(); // creamos la solucion vacia
+        this.unionFind = new UnionFind(this.estaciones.size()); // creamos el unionFind con la cant de estaciones
+        this.ordenarCandidatosPorMenorKM();// ordenamos los candidatos de menor a mayor
+
+        Iterator<Arco<Integer>> itCandidatos = this.candidatos.iterator(); // obtenemos un iterador de los candidatos
+
+        while(itCandidatos.hasNext() && solucion.size() <= this.estaciones.size() - 1) {
+
+            this.metrica++;
+            Arco<Integer> aux = itCandidatos.next(); // tomamos el primer candidato
+            this.candidatos.remove(aux);// y lo borramos
+            itCandidatos = this.candidatos.iterator(); // actualizamos el iterator luego de borrar el candidato seleccionado de la lista
+
+            // si no es un arco redundante
+            if(this.addArcoAccesible(aux)){
+
+                solucion.add(aux);
+                this.unionFind.union(this.estaciones.indexOf(aux.getVerticeOrigen()), this.estaciones.indexOf(aux.getVerticeDestino()));
+                this.kmActuales += aux.getEtiqueta();
+
+            }
+        }
+
+        return solucion;
+    }
+
+    // ordena los candidatos de menor a mayor segun la cant de km.
+    private void ordenarCandidatosPorMenorKM(){
+        for (int x = 0; x < this.candidatos.size(); x++) {
+            for (int y = 0; y < this.candidatos.size() - 1; y++) {
+                int elementoActual = this.candidatos.get(y).getEtiqueta();
+                int elementoSiguiente = this.candidatos.get(y + 1).getEtiqueta();
+                if (elementoActual > elementoSiguiente) {
+                    // Intercambiar
+                    Arco<Integer> sig = this.candidatos.get(y+1);
+                    Arco<Integer> actual = this.candidatos.get(y);
+                    this.candidatos.set(y,sig);
+                    this.candidatos.set(y + 1, actual);
+                }
+            }
+        }
+    }
+
+    // setea la lista de estaciones
+    private void getEstaciones() {
+        for (Arco<Integer> a : this.candidatos) {
+            if (!this.estaciones.contains(a.getVerticeOrigen())) {
+                this.estaciones.add(a.getVerticeOrigen());
+            }
+            if (!this.estaciones.contains(a.getVerticeDestino())) {
+                this.estaciones.add(a.getVerticeDestino());
+            }
+        }
+
+    }
+
+    // Metodo encargado de corroborar si un arco es redudante o no (osea si agrega una conexion con una estacion).
+    private boolean addArcoAccesible(Arco<Integer> tunel) {
+        int u = this.unionFind.find(this.estaciones.indexOf(tunel.getVerticeDestino()));
+        int v = this.unionFind.find(this.estaciones.indexOf(tunel.getVerticeOrigen()));
+
+        if (v != u) {
+            return true;
+        }
+        return false;
+    }
+}
